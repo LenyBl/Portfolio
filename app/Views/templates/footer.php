e1fad6305e37bae5fb2f4eb4de5e124c7617103d
Leny Blee <blee.leny@gmail.com>
2023-09-13 21:59:40 +0200

    footer finish

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,5 +1,6 @@
 <footer class="bg-white text-dark text-center py-3 ">
     <div class="container">
-        <p>&copy; 2023 Mon Site Web. Tous droits réservés.</p>
+        <p>&copy; 2023 Portfolio. Tous droits réservés.</p>
+        <p><a href="<?= base_url("/contact"); ?>">Me contacter</a></p>
     </div>
 </footer>
