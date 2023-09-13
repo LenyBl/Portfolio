e9a94b3aea58eb3f3730b10e7e1a66e6bff18d63
Leny Blee <blee.leny@gmail.com>
2023-09-13 17:41:31 +0200

    form contact + logo onglet

--- app/Views/pages/contact.php
+++ app/Views/pages/contact.php
@@ -1,14 +1,20 @@
-<div class="container-md">
-    <div class="input-group">
-        <span class="input-group-text">Nom et prénom</span>
-        <input type="text" aria-label="First name" class="form-control">
-        <input type="text" aria-label="Last name" class="form-control">
-    </div>
-    <div class="input-group mb-3">
-        <span class="input-group-text" id="basic-addon2">@example.com</span>
-        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username"
-            aria-describedby="basic-addon2">
 
-    </div>
-
+<div class="container position-absolute top-50 start-50 translate-middle rounded" style="max-width: 600px; background-color: white; padding-top: 15px;">
+    <form>
+        <div class="form-group mb-2">
+            <label for="nom" class="mb-3">Nom :</label>
+            <input type="text" class="form-control" id="nom" placeholder="Entrez votre nom">
+        </div>
+        <div class="form-group mb-2">
+            <label for="email" class="mb-3">Email :</label>
+            <input type="email" class="form-control" id="email" placeholder="Entrez votre email">
+        </div>
+        <div class="form-group mb-2">
+            <label for="message" class="mb-3">Message :</label>
+            <textarea class="form-control" style="max-height: 500px;" id="message" rows="5" placeholder="Entrez votre message"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary mb-3">Envoyer</button>
+    </form>
 </div>
+
+
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -7,13 +7,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
         <link rel="stylesheet" href="inc/style.css">
+        <link rel="icon" href="/img/portfolio.png">
     <title>Portfolio Blee Leny</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Blee Leny</a>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color : grey;">
+        <div class="container-fluid" >
+            <a class="navbar-brand" href="<?= base_url('/') ;?>">Blee Leny</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
                 aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -21,7 +22,7 @@
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">PROFIL</a>
+                        <a class="nav-link active" aria-current="page" href="<?= base_url('/') ;?>">PROFIL</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">BTS SIO</a>
